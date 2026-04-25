f071f85cdae64333c84aaad4110e0515f5f132d4
Tegar Ramadhan <ramadhantegar731@gmail.com>
2026-04-25 11:58:16 +0700

    fixing error

--- src/common/interface/authenticated-request.interface.ts
+++ src/common/interface/authenticated-request.interface.ts
@@ -1,9 +1,11 @@
+import { Role } from '@prisma/client';
 import { Request } from 'express';
 
 export interface RequestUser {
   user_id: string;
   user_username: string;
   user_name: string;
+  user_role: Role;
 }
 
 // Tetap butuh ini sebagai tipe di @Req()
