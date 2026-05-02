b271f75b4060086d1aaeb12a8f71083e5d723af6
Tegar Ramadhan <ramadhantegar731@gmail.com>
2026-05-03 02:31:07 +0700

    adding error for user friendly

--- src/main.ts
+++ src/main.ts
@@ -34,8 +34,8 @@ async function bootstrap() {
     }),
   );
 
-  app.useGlobalFilters(new HttpExceptionFilter());
   app.setGlobalPrefix('api');
+  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(4000);
   console.log('Server running on http://localhost:4000/');
