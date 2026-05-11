3d74eec9b9056f971535ecc42689a9ec2c8d6f8e
Tegar Ramadhan <ramadhantegar731@gmail.com>
2026-05-11 17:04:29 +0700

    protect route location

--- src/location/location.controller.ts
+++ src/location/location.controller.ts
@@ -6,21 +6,20 @@ import {
   HttpCode,
   HttpStatus,
   Param,
+  ParseUUIDPipe,
   Patch,
   Post,
+  UseGuards,
 } from '@nestjs/common';
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard';
+import { Roles, RolesGuard } from '../auth/guards/roles.guard';
 import { CreateLocationDto, UpdateLocationDto } from './dto/location.dto';
 import { LocationService } from './location.service';
 
+// ── PUBLIC ────────────────────────────────────────────────────────────────────
 @Controller('locations')
 export class LocationController {
   constructor(private readonly locationService: LocationService) {}
-
-  @Post()
-  @HttpCode(HttpStatus.CREATED)
-  create(@Body() createLocationDto: CreateLocationDto) {
-    return this.locationService.create(createLocationDto);
-  }
 
   @Get()
   @HttpCode(HttpStatus.OK)
@@ -30,22 +29,36 @@ export class LocationController {
 
   @Get(':id')
   @HttpCode(HttpStatus.OK)
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
     return this.locationService.findOne(id);
+  }
+}
+
+// ── ADMIN ─────────────────────────────────────────────────────────────────────
+@Controller('admin/locations')
+@UseGuards(JwtAuthGuard, RolesGuard)
+@Roles('ADMIN')
+export class LocationAdminController {
+  constructor(private readonly locationService: LocationService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  create(@Body() dto: CreateLocationDto) {
+    return this.locationService.create(dto);
   }
 
   @Patch(':id')
   @HttpCode(HttpStatus.OK)
   update(
-    @Param('id') id: string,
-    @Body() updateLocationDto: UpdateLocationDto,
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() dto: UpdateLocationDto,
   ) {
-    return this.locationService.update(id, updateLocationDto);
+    return this.locationService.update(id, dto);
   }
 
   @Delete(':id')
   @HttpCode(HttpStatus.OK)
-  remove(@Param('id') id: string) {
+  remove(@Param('id', ParseUUIDPipe) id: string) {
     return this.locationService.remove(id);
   }
 }
--- src/location/location.module.ts
+++ src/location/location.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { LocationController } from './location.controller';
+import {
+  LocationAdminController,
+  LocationController,
+} from './location.controller';
 import { LocationService } from './location.service';
 
 @Module({
   imports: [PrismaModule],
-  controllers: [LocationController],
+  controllers: [LocationController, LocationAdminController],
   providers: [LocationService],
   exports: [LocationService],
 })
